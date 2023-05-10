f5a80e9cc0496cd07e19c9d78293273ac09ddaa2
dmitriy-student <dima.ermol22@gmail.com>
2023-05-11 00:29:24 +0300

    add css

--- src/js/home.js
+++ src/js/home.js
@@ -1,11 +1,12 @@
 import { apiRequestTopBooks } from './queries';
 import { createMarkupTopBooks } from './create-markup-home';
 
-const mainContainer = document.querySelector('.main-container');
+const mainContainerEl = document.querySelector('.js-main-container');
+const seeMoreBtn = document.querySelector('.js-top-books_button');
 
 apiRequestTopBooks()
   .then(data => {
-    mainContainer.insertAdjacentHTML('beforeend', createMarkupTopBooks(data));
+    mainContainerEl.insertAdjacentHTML('beforeend', createMarkupTopBooks(data));
     if (data.page !== data.total_pages) {
       paginationBtn.hidden = false;
     }
