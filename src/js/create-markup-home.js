8793f3f639a6b9bff0caef3fb39cd9eb147aeda9
dmitriy-student <dima.ermol22@gmail.com>
2023-05-11 21:51:06 +0300

    add button on book card

--- src/js/create-markup-home.js
+++ src/js/create-markup-home.js
@@ -6,11 +6,12 @@ function createMarkupTopBooks(arr) {
       const booksMarkup = obj.books
         .map(
           obj => `<li class="top-books_list list">
-    <a href="#" class="top-books_link link">
+    <div class="top-books_book">
         <img src="${obj.book_image}" alt="book_image"  width="180">
         <h3 class="top-books_title">${obj.title}</h3>
         <p class="top-books_author">${obj.contributor}</p>
-    </a>
+        <div class='top-books_quick-view hiden' data-bookId='${obj._id}'>quick view</div>
+    </div>
 </li>`
         )
         .join('');
