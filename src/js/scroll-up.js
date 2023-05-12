a54120727143651c670c1145eab2eb45dea71f7c
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-05-12 09:16:49 +0300

    Update scroll-up.js

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,22 +1,16 @@
-// const ch = document.querySelector('body');
-// if (window.pageYOffset > 1) {
-//   ch.insertAdjacentHTML(
-//     'afterend',
-//     `<div class="to-top">
-//       <svg width="55" height="55">
-//         <use href="./images/icons/icons.svg#icon-instagram"></use>
-//       </svg>
-//     </div>`
-//   )
-//   // setTimeout(toTopBtn, 0);
-// }
-// выбираем элемент, который хотим добавить
-// создаем новый элемент div с классом "to-top"
-
 const gotopBTN = document.querySelector('.go-top');
 
 gotopBTN.addEventListener('click', onToTopBtn);
+
+function onToTopBtn() {
+  if (window.pageYOffset > 0) {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    // setTimeout(toTopBtn, 0);
+  }
+}
+
 window.addEventListener('scroll', trackScroll);
+
 function trackScroll() {
   const offset = window.pageYOffset;
   const coords = document.documentElement.clientHeight;
@@ -24,21 +18,5 @@ function trackScroll() {
     gotopBTN.classList.add('go-top-show');
   } else {
     gotopBTN.classList.remove('go-top-show');
-  }
-}
-// function onScroll() {
-//   const scrolled = window.pageYOffset;
-//   const coords = document.documentElement.clientHeight;
-//   if (scrolled > coords) {
-//     toTopBtn.classList.add('btn-to-top--visible');
-//   }
-//   if (scrolled < coords) {
-//     toTopBtn.classList.remove('btn-to-top--visible');
-//   }
-// }
-function onToTopBtn() {
-  if (window.pageYOffset > 0) {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-    // setTimeout(toTopBtn, 0);
   }
 }
