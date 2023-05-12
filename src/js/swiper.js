28a38d4a0956c4e4c9447078242bb18e89a0a8b6
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-12 19:25:55 +0300

    swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,19 +1,19 @@
-import Swiper from 'swiper';
-import 'swiper/swiper.min.css';
-import 'swiper/swiper-bundle.min.css';
+// import Swiper from 'swiper';
+// import 'swiper/swiper.min.css';
+// import 'swiper/swiper-bundle.min.css';
 // import 'swiper/css/navigation';
 import { funds } from './funds';
 
-const list_support = document.querySelector('.swiper-slide');
+const list_support = document.querySelector('.support-cont-wrapper');
 
 function createFondMarkup(arr) {
   const markup = arr
     .map(
       ({ number, img, url, title }) =>
-        `<a class="link support_funds_element" href="${url}" target="_blank" rel="noopener noreferrer">
+        `<div class="swiper-slide support-cont-slide"><a class="link support_funds_element" href="${url}" target="_blank" rel="noopener noreferrer">
     <span class="support_funds_number">${number}</span>
     <img class="support_funds_image" src="${img}" alt="${title}">
-</a>`
+</a></div>`
     )
     .join('');
   return markup;
@@ -21,19 +21,41 @@ function createFondMarkup(arr) {
 
 list_support.insertAdjacentHTML('beforeend', createFondMarkup(funds));
 
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  slidesPerView: 4,
+// const swiper = new Swiper('.swiper', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+//   slidesPerView: 4,
 
-  // Navigation arrows
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.slider__next',
+//   },
+
+//   // And if we need scrollbar
+//   //   scrollbar: {
+//   //     el: '.swiper-scrollbar',
+//   //   },
+// });
+//==================================
+new Swiper('.support-cont', {
   navigation: {
-    nextEl: '.slider__next',
+    // nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  //   scrollbar: {
-  //     el: '.swiper-scrollbar',
-  //   },
+  slidesPerView: 4,
+  spaceBetween: 20,
+  loop: true,
+  freeMode: true,
+  direction: 'vertical',
+  breakpoints: {
+    320: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1440: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
 });
