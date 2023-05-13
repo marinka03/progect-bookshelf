ebee53619f24f3bf2d37d421563d8f5bf8f93915
Yevgeniy Utkin <yevgeniy.utkin@gmail.com>
2023-05-13 22:12:17 +0300

    add LocalStorage

--- src/shopping.js
+++ src/shopping.js
@@ -1,8 +1,8 @@
 import './js/queries';
 import './js/home';
-import './js/create-markup-home';
-// import './js/shopping-list';
-// import './js/create-markup-shopping';
+// import './js/create-markup-home';
+import './js/shopping-list';
+import './js/create-markup-shopping';
 import './js/btn';
 import './js/funds';
 import './js/support-ukraine';
