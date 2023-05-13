846af6c6a75a10c91bf36603a540189537347629
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-05-13 14:18:49 +0300

    fix loader

--- src/js/queries.js
+++ src/js/queries.js
@@ -1,12 +1,68 @@
+// export { apiRequestTopBooks };
+
+// function apiRequestTopBooks() {
+//   const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
+
+//   return fetch(BASE_URL).then(resp => {
+//     if (!resp.ok) {
+//       throw new Error(resp.statusText);
+//     }
+//     return resp.json();
+//   });
+// }
 export { apiRequestTopBooks };
 
 function apiRequestTopBooks() {
   const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
-
-  return fetch(BASE_URL).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
+  showLoader();
+  return fetch(BASE_URL)
+    .then(resp => {
+      hideLoader();
+      if (!resp.ok) {
+        showLoader();
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .catch(error => {
+      showLoader();
+      console.error(error);
+    });
 }
+
+// function apiRequestTopBooks() {
+//   const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
+//   showLoader();
+//   return fetch(BASE_URL)
+//     .then(resp => {
+//       hideLoader();
+//       if (!resp.ok) {
+//         showLoader();
+//         throw new Error(resp.statusText);
+//       }
+//       return resp.json();
+//     })
+//     .catch(error => {
+//       showLoader();
+//       console.error(error);
+//     });
+
+function showLoader() {
+  const preloader = document.querySelector('#preloader');
+  preloader.classList.remove('hide');
+}
+
+function hideLoader() {
+  const preloader = document.querySelector('#preloader');
+  preloader.classList.add('hide');
+}
+
+// // вызываем функцию apiRequestTopBooks() для получения данных из API
+// apiRequestTopBooks().then(data => {
+//   // вызываем функцию createMarkupTopBooks() для создания разметки на странице
+//   const markup = createMarkupTopBooks(data);
+//   // находим контейнер для разметки на странице
+//   const container = document.querySelector('#top-books_container');
+//   // вставляем разметку в контейнер
+//   container.innerHTML = markup;
+// });
