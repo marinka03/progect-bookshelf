a57ed3e9f8431ee8cf98e9f94845b924e809eedf
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-15 17:05:29 +0300

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,42 @@
+export const refs = {
+  //category-list
+  categoryListEl: document.querySelector('.category-list'),
+
+  //create-modal
+  bookQuikWiew: document.querySelector('.main'),
+  modalElement: document.querySelector('.modal_pop-up__container'),
+  modalInfo: document.querySelector('.modal_pop-up__description'),
+  bookDescription: document.querySelector('.no-description'),
+  backdrop: document.querySelector('.backdrop_pop-up'),
+  closeModalBtn: document.querySelector('.modal_pop-up__close-btn'),
+
+  //header-current-page
+  homeCurrentPage: document.querySelector('.header__home-box'),
+  shoppingCurrentPage: document.querySelector('.header__shopping-box'),
+
+  //home
+  mainHomeContainerEl: document.querySelector('.js-section-top-books'),
+
+  //login-modal
+  modalContainer: document.querySelector('.modal'),
+  openModalButton: document.querySelector('.header__sign-up'),
+  backdrop: document.querySelector('.modal-overlay'),
+  nameInput: document.querySelector('.name'),
+  sighnUpOpt: document.getElementById('sighn-up-opt'),
+  sighnInOpt: document.getElementById('sighn-in-opt'),
+  menu: document.querySelector('.header__menu'),
+
+  form: document.querySelector('.form'),
+  email: document.getElementById('email'),
+  password: document.getElementById('password'),
+  name: document.getElementById('name'),
+  sihbUpSvg: document.querySelector('.header__svg-array'),
+  sihnInSvg: document.querySelector('.header__svg-array-down'),
+
+  userImg: document.querySelector('.user-img'),
+
+  //menu
+  burger: document.querySelector('.burger-box'),
+  iconClose: document.querySelector('.close-box'),
+  burgerMenu: document.querySelector('.burger-menu'),
+};
