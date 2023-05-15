fc8574240006e92f43ea95330ebd6e99ded97ec8
Stas Voievodin <vstasv@i.ua>
2023-05-15 17:39:13 +0300

    category functional without  css
    зробив функціонал списку категорій, але без сіесес

--- src/js/category-list.js
+++ src/js/category-list.js
@@ -1,7 +1,25 @@
-
+import { createMarkupCategory } from './create-markup-category-books';
+import { createMarkupTopBooks } from './create-markup-home'
+import { apiRequestTopBooks } from './queries';
 
 const categoryListEl = document.querySelector('.category-list');
-console.log(categoryListEl);
+const categoryConteiner = document.querySelector('.js-top-books-container');
+const categoryTitle = document.querySelector('h1')
+console.dir(categoryTitle)
+const allCategoryEl = document.querySelector('.all-category');
+
+
+// allCategoryEl.addEventListener('click',   );
+categoryListEl.addEventListener('click', async (e) => {
+    // if (e.target.tagName === 'UL') return;
+    const categoryRequest = e.target.textContent;
+    data = await apiRequestByCategory(categoryRequest);
+    categoryConteiner.innerHTML = createMarkupCategory(data);
+    categoryTitle.innerText = `${e.target.textContent}`
+    // console.log(data);
+});
+
+
 
 apiRequestCategoriesList()
     .then(data => {
@@ -23,6 +41,15 @@ function apiRequestCategoriesList() {
     });
 }
 
+function apiRequestByCategory(category) {
+    const CATEGORY_URL = `https://books-backend.p.goit.global/books/category?category=${category}`;
+    return fetch(CATEGORY_URL).then(resp => {
+        if (!resp.ok) {
+            throw new Error(resp.statusText);
+        }
+        return resp.json();
+    });
+}
 
 
 function createMarkupCategList(arr) {
