35099303ed4bf5a903fb87eae5880b0fc50a7d0b
dmitriy-student <dima.ermol22@gmail.com>
2023-05-15 23:39:49 +0300

    edit category list function

--- src/js/home.js
+++ src/js/home.js
@@ -2,19 +2,23 @@ import { apiRequestTopBooks } from './queries';
 import { createMarkupTopBooks } from './create-markup-home';
 import { apiRequestCategory } from './queries';
 import { createMarkupCategory } from './create-markup-category-books';
-
+export { allCategoryCreate };
 const mainHomeContainerEl = document.querySelector('.js-section-top-books');
 
-apiRequestTopBooks()
-  .then(data => {
-    const MarkupTopBooks = createMarkupTopBooks(data);
-    mainHomeContainerEl.innerHTML = MarkupTopBooks;
+function allCategoryCreate() {
+  apiRequestTopBooks()
+    .then(data => {
+      const MarkupTopBooks = createMarkupTopBooks(data);
+      mainHomeContainerEl.innerHTML = MarkupTopBooks;
 
-    if (data.page !== data.total_pages) {
-      paginationBtn.hidden = false;
-    }
-  })
-  .catch(err => console.log(err));
+      if (data.page !== data.total_pages) {
+        paginationBtn.hidden = false;
+      }
+    })
+    .catch(err => console.log(err));
+}
+
+allCategoryCreate();
 
 document.body.addEventListener('click', function (event) {
   if (event.target.classList.contains('js-top-books_button')) {
