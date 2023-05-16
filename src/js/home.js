7e83b62e9b49cb461622bb170b9cf00537f35802
dmitriy-student <dima.ermol22@gmail.com>
2023-05-16 20:32:14 +0300

    Done

--- src/js/home.js
+++ src/js/home.js
@@ -2,7 +2,6 @@ import { apiRequestTopBooks } from './queries';
 import { createMarkupTopBooks } from './create-markup-home';
 import { apiRequestCategory } from './queries';
 import { createMarkupCategory } from './create-markup-category-books';
-import { save, load, remove } from './storage-servises';
 import { saveActiveCategory, changeActiveCategory } from './category-list';
 
 export { allCategoryCreate };
@@ -19,12 +18,27 @@ function allCategoryCreate() {
       }
       // сброс активной категории
       saveActiveCategory('All categories');
+      changeActiveCategory();
     })
     .catch(err => console.log(err));
 }
 
 allCategoryCreate();
-// changeActiveCategory();
+
+// const seeMoreBtn = document.querySelector('.js-top-books_button');
+
+// seeMoreBtn.addEventListener('click', () => {
+//   apiRequestCategory(category)
+//     .then(data => {
+//       console.log('data :>> ', data);
+//       const MarkupCategory = createMarkupCategory(data);
+//       mainHomeContainerEl.innerHTML = `<h1 class="title-top-books">${category}</h1><div class="category-books_container">${MarkupCategory}</div>`;
+//       // добавление активной категории
+//       saveActiveCategory(category);
+//       changeActiveCategory();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 document.body.addEventListener('click', function (event) {
   if (event.target.classList.contains('js-top-books_button')) {
