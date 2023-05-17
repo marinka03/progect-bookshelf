9891b9312ce73b2a49756a2c4be21a686e515725
Yevgeniy Utkin <yevgeniy.utkin@gmail.com>
2023-05-17 18:03:25 +0300

    add tui

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -2,6 +2,9 @@ import { hideLoader } from './queries';
 import { generateCard } from './create-markup-shopping';
 import shoppping_list from '../images/shopping_list.png';
 
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
 import { generateCard, generateCards } from './create-markup-shopping';
 
 import 'firebase/auth';
@@ -20,6 +23,31 @@ console.log(localStorageEL.books);
 // listEl.insertAdjacentHTML('beforeend', createMarkupBooksInShopping(bookList));
 // const btnDelete = document.querySelector('.js-li-shopping');
 
+const options = {
+  totalItems: localStorageEL.books.length,
+  itemsPerPage: 3,
+  visiblePages: 3,
+  page: 1,
+  centerAlign: true,
+  //   template: {
+  //     // Переопределение шаблона компонентов
+  //     page: '<li class="pagination-item"><a href="#" class="page-link">{{page}}</a></li>',
+  //     currentPage:
+  //       '<li class="pagination-item active"><a href="#" class="page-link">{{page}}</a></li>',
+  //     moveButton:
+  //       '<li class="pagination-item"><a href="#" class="page-link-{{type}}">' +
+  //       '<span class="tui-ico-{{type}}">{{type}}</span></a></li>',
+  //     disabledMoveButton:
+  //       '<li class="pagination-item disabled"><a href="#" class="page-link-{{type}}">' +
+  //       '<span class="tui-ico-{{type}}">{{type}}</span></a></li>',
+  //   },
+  //   onPageChange: function (pageNum) {
+  //     console.log('Page changed:', pageNum);
+  //   },
+};
+
+const pagination = new Pagination('pagination', options);
+
 const empty = emptyWrapper;
 console.log(localStorageEL);
 console.log(Object.keys(localStorageEL).length);
@@ -36,13 +64,136 @@ if (!Object.keys(localStorageEL).length) {
 }
 
 const array = [];
+// localStorageEL.books.forEach(item => {
+//   apiFetchCate(item).then(data => {
+//     console.log(data);
+//     array.push(data);
+//     listEl.insertAdjacentHTML('beforeend', generateCard(data));
+//   });
+// });
+
+let count = 0;
 localStorageEL.books.forEach(item => {
-  apiFetchCate(item).then(data => {
-    console.log(data);
-    array.push(data);
-    listEl.insertAdjacentHTML('beforeend', generateCard(data));
-  });
+  //   console.log(count);
+  count += 1;
+  if (!(count > 3)) {
+    apiFetchCate(item).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+  }
 });
+
+// const array = [];
+// pagination.on('beforeMove', evt => {
+//   //   localStorageEL.books.forEach(item => {
+//   //     apiFetchCate(item).then(data => {
+//   //       console.log(data);
+//   //       array.push(data);
+//   //       listEl.innerHTML = generateCard(data);
+//   //     });
+//   //   });
+//   console.log(evt);
+// });
+
+function paginationPage(page) {
+  //   console.log(page);
+  for (let i = 0; i < 3; i++) {
+    console.log(page);
+  }
+}
+pagination.on('beforeMove', evt => {
+  //   listEl.innerHTML = '';
+  //   let count = 0;
+  //   localStorageEL.books.forEach(item => {
+  //     // console.log(count);
+  //     count += 1;
+  //     if (!(count > 3)) {
+  //       apiFetchCate(item).then(data => {
+  //         console.log(data);
+  //         array.push(data);
+  //         listEl.insertAdjacentHTML('beforeend', generateCard(data));
+  //       });
+  //     }
+  //   });
+  console.log(localStorageEL.books);
+
+  paginationPage(evt.page);
+
+  if (evt.page === 1) {
+    listEl.innerHTML = '';
+    apiFetchCate(localStorageEL.books[0]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[1]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[2]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+  } else if (evt.page === 2) {
+    listEl.innerHTML = '';
+    apiFetchCate(localStorageEL.books[3]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[4]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[5]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+  } else if (evt.page === 3) {
+    listEl.innerHTML = '';
+    apiFetchCate(localStorageEL.books[6]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[7]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[8]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+  } else if (evt.page === 4) {
+    listEl.innerHTML = '';
+    apiFetchCate(localStorageEL.books[9]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[10]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+    apiFetchCate(localStorageEL.books[11]).then(data => {
+      console.log(data);
+      array.push(data);
+      listEl.insertAdjacentHTML('beforeend', generateCard(data));
+    });
+  }
+  console.log(evt);
+});
+
+// pagination.on('afterMove', ({ page }) => console.log(page));
 
 listEl.addEventListener('click', onClickBtnDelete);
 
@@ -80,12 +231,12 @@ function onClickBtnDelete(evt) {
     listEl.innerHTML = generateCards(array);
 
     if (!array.length) {
-      console.log(emptyWrapper.style.display);
+      //   console.log(emptyWrapper.style.display);
       //   emptyWrapper.style.display = 'block';
-      console.log(emptyWrapper.style.display);
-      console.log(emptyWrapper);
-      console.log(empty);
-      console.log('1');
+      //   console.log(emptyWrapper.style.display);
+      //   console.log(emptyWrapper);
+      //   console.log(empty);
+      //   console.log('1');
       //   addABook.style.display = 'none';
       //   listEl.insertAdjacentElement('beforeend', emptyWrapper);
       listEl.innerHTML = `<div class="wrapper-empty-page js-wrapper-empty-page"><p class="wrapper-empty-page_description js-descr-empty">This page is empty, add some books and proceed to order.</p><img class="js-image-empty" src="${shoppping_list}" alt="This page is empty" /></div>`;
