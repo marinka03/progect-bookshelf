7d615d13f18ce1d2c438a9f6d85b656cb43b1074
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-05-17 10:22:23 +0300

    оптимізація теми

--- src/js/theme.js
+++ src/js/theme.js
@@ -8,15 +8,6 @@ const STORAGE_KEY = 'themeKey';
 const checkBox = document.querySelector('#toggle');
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
-const categoryListName = document.querySelector('.category-list');
-const headerHome = document.querySelector('.header__home');
-const headerShopping = document.querySelector('.header__shopping');
-const currentHome = document.querySelector('.current-home');
-const currentShopping = document.querySelector('.current-shopping');
-const modalPopUp = document.querySelector('.modal_pop-up');
-const burger = document.querySelector('.burger');
-const iconClose = document.querySelector('.icon-close');
-const main = document.querySelector('main');
 
 checkBox.addEventListener('change', onChange);
 
@@ -25,44 +16,12 @@ function onChange(e) {
     body.classList.remove('ligth-theme');
     body.classList.add('dark-theme');
     header.classList.add('dark-theme_header');
-    // headerHome.classList.add('dark-theme_header-hover');
-    // headerShopping.classList.add('dark-theme_header-hover');
-    // if (headerHome.classList.contains('current-home')) {
-    //   currentHome.classList.add('dark-theme-curent-page');
-    // }
-    // currentShopping.classList.add('dark-theme-curent-page');
-    // if (headerHome.classList.contains('current-home')) {
-    //   headerShopping.classList.add('dark-theme_header-hover');
-    // }
-    // if (currentShopping.classList.contains('dark-theme-curent-page')) {
-    //   headerHome.classList.add('dark-theme_header-hover');
-    // }
-    burger.classList.add('dark-burger-box');
-    iconClose.classList.add('dark-burger-box');
-    // if (
-    //   main.classList.contains('global-container') ||
-    //   categoryListName.classList.contains('light-theme-color-item')
-    // ) {
-    // categoryListName.classList.remove('light-theme-color-item');
-    // categoryListName.classList.add('dark-theme-category-list_item');
-    // }
-
-    // if (body.classList.contains('dark-theme')) {
-    //   modalPopUp.classList.add('dark-theme_modal-book');
-    // }
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(Theme.DARK));
   } else {
     body.classList.add('ligth-theme');
     body.classList.remove('dark-theme');
     header.classList.remove('dark-theme_header');
-    // headerHome.classList.remove('dark-theme_header-hover');
-    // currentHome.classList.remove('dark-theme-curent-page');
-    burger.classList.remove('dark-burger-box');
-    iconClose.classList.remove('dark-burger-box');
-    headerShopping.classList.remove('dark-theme_header-hover');
-    // categoryListName.classList.remove('dark-theme-category-list_item');
-    // categoryListName.classList.add('light-theme-color-item');
     localStorage.setItem(STORAGE_KEY, JSON.stringify(Theme.LIGHT));
   }
 }
@@ -77,17 +36,6 @@ function isTheme() {
     if (parseTheme === 'dark-theme') {
       body.classList.add('dark-theme');
       header.classList.add('dark-theme_header');
-      // headerHome.classList.add('dark-theme_header-hover');
-      if (headerHome.classList.contains('current-home')) {
-        currentHome.classList.add('dark-theme-curent-page');
-      }
-      burger.classList.add('dark-burger-box');
-      iconClose.classList.remove('dark-burger-box');
-      // headerShopping.classList.add('dark-theme_header-hover');
-      // if (main.classList.contains('global-container')) {
-      // categoryListName.classList.remove('light-theme-color-item');
-      // categoryListName.classList.add('dark-theme-category-list_item');
-      // }
 
       checkBox.checked = true;
     }
